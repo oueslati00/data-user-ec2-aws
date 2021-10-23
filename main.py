940e94780d50c262e93bb56ea7540f62e19e666e
brahim oueslati <52589090+oueslati00@users.noreply.github.com>
2021-10-23 14:59:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+
+
 from fastapi import FastAPI
-app= FastAPI()
+
+app = FastAPI()
+
+
 @app.get("/")
 async def root():
-    return {"message" : "Hello Word"}
+    return {"message": "Hello World"}
+
